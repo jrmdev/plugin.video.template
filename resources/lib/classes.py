fa089a054b96390b40ff26f2d7a1d9f8da2b0671
jrmdev <jrmdev@github.com>
2020-05-16 17:08:33 +1000

    minor update

--- resources/lib/classes.py
+++ resources/lib/classes.py
@@ -1,5 +1,6 @@
 import urllib
 import utils
+import config
 
 class MenuItem(object):
     def __init__(self, **kwargs):
@@ -12,15 +13,23 @@ class MenuItem(object):
         return hasattr(self, attr) and getattr(self, attr)
 
     def get(self, attr):
-        return getattr(self, attr) if self.has(attr) else None
+        if not self.has(attr):
+            return None
+
+        val = getattr(self, attr)
+
+        if isinstance(val, str) and val.startswith(('http://', 'https://')):
+            return '{}|{}'.format(val, urllib.urlencode({'User-Agent': config.USER_AGENT, 'Range': ''}))
+
+        return val
 
     def to_kodi_url(self):
         # Make sure encoding is correct for displaying in kodi menus
         for k in self.__dict__:
-            var = getattr(self, k)
+            val = getattr(self, k)
             
-            if isinstance(var, unicode):
-                setattr(self, k, var.encode('utf-8', 'ignore'))
+            if isinstance(val, unicode):
+                setattr(self, k, val.encode('utf-8', 'ignore'))
 
         # If we clicked on a playable item, just include the url
         if self.playable and self.has('url'):
